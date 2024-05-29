c573eb758f60003ef9789f132f708a423a8001f9
Jeff Schueler <jschuel@hawaii.edu>
2024-05-29 10:41:57 -0700

    Updated setup.py to include python 3.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.9',
     install_requires=[
         "label-studio>=1.12.1",
         "label-studio-ml>=1.0.9",
